edf228e0e54cd1c8e50c06967245f31e3c21bb7c
shani723327 <safdarshan723327@gmail.com>
2026-05-05 21:10:56 +0500

    Update hero.py

--- hero.py
+++ hero.py
@@ -384,7 +384,7 @@ with ShaniXD(max_workers=50) as Shani:
             uid = mal
             Shani.submit(____old____, uid, total_ids)
     print('');linex();print(f"\n{green} Cloning Session Complete")
-    print(f"{white}➤ Total OK: {green}{len(ok)}")
+	print(f"{white}➤ Total OK: {green}{len(ok)}")
     print(f"{white}➤ Total CP: {red}{len(cp)}")
     linex()
     exit()
