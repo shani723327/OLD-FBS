8f6e4eecbfb579759151fa057d681a947ff37e54
shani723327 <safdarshan723327@gmail.com>
2026-05-05 20:57:37 +0500

    Update hero.py

--- hero.py
+++ hero.py
@@ -374,9 +374,10 @@ def __2010___2011():
     for i in range(int(limit)):
         data = random.choice(["100001","100002","100003","100004"])+str(random.choice(range(111111111, 999999999)))
         user.append(data)
-    with ShaniXD(max_workers=50) as Shani:
-		total_ids = int(limit) 
-		clear()
+		
+		with ShaniXD(max_workers=50) as Shani:
+        clear()
+        total_ids = int(limit)
         print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
         print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
         linex()
